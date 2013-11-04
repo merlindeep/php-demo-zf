44b818ca1a6f2bcea080c8e22332581a7e9ca687
merlindeep <merlindeep@gmail.com>
2013-11-05 00:20:12 +0400

    init simple zend framework application; add nginx development config

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+define('APPCONFIG_FILE_NAME', APPLICATION_PATH . '/configs/application.ini');
+
+define('APPLICATION_ENV', 'production');
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+require_once 'Zend/Loader/Autoloader.php';
+Zend_Loader_Autoloader::getInstance();
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPCONFIG_FILE_NAME
+);
+
+
+$application->bootstrap()
+            ->run();
